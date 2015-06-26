77039a93a32b40245df7bb070b3f28cc96d41d10
virus <virusbb001a@gmail.com>
2015-06-26 17:43:47 +0900

    大幅に変更

--- myapp.rb
+++ myapp.rb
@@ -1,8 +1,14 @@
+#!/usr/bin/env ruby
+#
 require 'sinatra'
 require 'sinatra/reloader'
 require 'json'
 require 'slim'
 require 'pp'
+
+configure :production do
+  enable :reloader
+end
 
 MinecraftHome="/Users/virus/Library/Application Support/minecraft"
 profiles=JSON.parse(
@@ -14,11 +20,47 @@ get '/' do
   slim :index
 end
 
-get '/show' do
+get '/profile' do
   redirect to('/') unless params[:name]
   if profiles["profiles"].key?(params[:name])
     @profile=profiles["profiles"][params[:name]]
-    slim :show
+    slim :profile
+  else
+    slim :error, locals: {error_mes: "Not found such profile: #{params[:name]}"}
+  end
+end
+
+get '/modlist' do
+  if profiles["profiles"].key?(params[:name])
+    @profile=profiles["profiles"][params[:name]]
+    if @profile.key?("gameDir") && FileTest.directory?("#{@profile["gameDir"]}/mods")
+      slim :error, locals: {error_mes: "This profile doesn't have mod dir: #{params[:name]}"}
+    end
+    @moddir=Dir.new("#{@profile["gameDir"]}/mods")
+    @modfiles=@moddir.each.drop_while { |i| i=~/^\.{1,2}$/ }
+    slim :modlist
+  else
+    slim :error, locals: {error_mes: "Not found such profile: #{params[:name]}"}
+  end
+end
+
+get '/options' do
+  if profiles["profiles"].key?(params[:name])
+    @profile=profiles["profiles"][params[:name]]
+    @gamedir=@profile["gameDir"] || MinecraftHome
+    @optionfile= @gamedir + "/options.txt"
+    @options={}
+    if File.exist?(@optionfile)
+      File.open(@optionfile,"r") do |file|
+        file.each_line do |line|
+          key,value = line.split(":")
+          @options[key]=value
+        end
+      end
+      slim :options
+    else
+      slim:error, locals: {error_mes: "File not found: #{@optionfile}" }
+    end
   else
     slim :error, locals: {error_mes: "Not found such profile: #{params[:name]}"}
   end
